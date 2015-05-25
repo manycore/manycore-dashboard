8ba066a37ddaadd44007f37dfb021256c6ad9529
Bérenger <berenger@arnaud.xyz>
2015-05-25 16:27:58 +0100

    Add admin route

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', pages);
 app.use('/service/profiles', profiles);
 app.use('/service/details', details);
+app.use('/service/admin', require('./routes/admin'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

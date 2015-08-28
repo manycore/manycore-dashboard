a9ec013f480fac8e0f301f7528b64b52efd08c52
Bérenger <berenger@arnaud.xyz>
2015-08-28 12:21:12 +0100

    Remove percent in stats :(

--- public/details/aside/aside.directive.js
+++ public/details/aside/aside.directive.js
@@ -13,12 +13,11 @@ var statLayout = function() {
 	
 	// Constants
 	this.stacks =		{ width: 10, padding: 4 };
-	this.percents =		{ width: 3, padding: 1 };
 	this.lineHeight =	40;
 
 	// Compute
 	this.refresh = function(deck, profiles) {
-		self.width =	(profiles.length == 1) ? self.stacks.width : 2 * (self.stacks.width + self.stacks.padding + self.percents.width) + self.percents.padding;
+		self.width =	(profiles.length == 1) ? self.stacks.width : self.stacks.width * 2 + self.stacks.padding;
 		self.height =	self.lineHeight * deck.length;
 	};
 };
@@ -68,26 +67,17 @@ app.directive('chartStats', function() {
 			
 		// Groups
 		var groupS = [];
-		var groupP = [];
 		groupS.push(svg.append("g")
 			.attr('class', "svg-stack svg-profile-1"));
-		if (profiles.length == 2) {
-			groupP.push(svg.append("g")
-				.attr('class', "svg-percent svg-profile-1")
-				.attr("transform", "translate(" + (layout.stacks.width + layout.stacks.padding) + ", 0)"));
+		if (profiles.length == 2)
 			groupS.push(svg.append("g")
 				.attr('class', "svg-stack svg-profile-2")
-				.attr("transform", "translate(" + (layout.stacks.width + 2 * layout.stacks.padding + 2 * layout.percents.width + layout.percents.padding) + ", 0)"));
-			groupP.push(svg.append("g")
-				.attr('class', "svg-percent svg-profile-2")
-				.attr("transform", "translate(" + (layout.stacks.width + layout.stacks.padding + layout.percents.width + layout.percents.padding) + ", 0)"));
-		}
+				.attr("transform", "translate(" + (layout.stacks.width + layout.stacks.padding) + ", 0)"));
 
 		// Draw rectanges
 		var yValue, yPrevious;
 		for (var index = 0; index < profiles.length; index++) {
 			stats.values[index].forEach(function(element, i) {
-				// Stack
 				yValue = layout.height * element.value / stats.maxSum;
 				yPrevious = layout.height * element.previous / stats.maxSum;
 				groupS[index].append("rect")
@@ -96,18 +86,6 @@ app.directive('chartStats', function() {
 					.attr("y", layout.height - yPrevious - yValue)
 					.attr("height", yValue)
 					.style("fill", deck[i].color)
-				
-				// Percent
-				if (profiles.length == 2) {
-					yValue = layout.height * element.value / stats.sums[index];
-					yPrevious = layout.height * element.previous / stats.sums[index];
-					groupP[index].append("rect")
-						.attr("width", layout.percents.width)
-						.attr("x", 0)
-						.attr("y", layout.height - yPrevious - yValue)
-						.attr("height", yValue)
-						.style("fill", deck[i].color);
-				}
 			}, this);
 		}
 	};

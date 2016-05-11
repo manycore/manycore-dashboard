a191945c196fc30006788e4955cb16972fdd2465
Bérenger <berenger@arnaud.xyz>
2016-05-11 17:54:14 +0100

    XP: add feedback for charts and comments

--- public_xp/page/feedback.controller.js
+++ public_xp/page/feedback.controller.js
@@ -40,8 +40,8 @@ xpapp.controller('FeedbackController', ['$controller', '$scope', '$rootScope', '
 	$scope.tabs = [
 		{ l: 'usability',		t: 'Usability' },
 		{ l: 'usefulness',		t: 'Usefulness' },
-		{ l: 'taxonomy',		t: 'Taxonomy' },
-		{ l: 'charts',			t: 'Charts' },
+		{ l: 'taxonomy',		t: 'Taxonomy (categories)' },
+		{ l: 'graphs',			t: 'Graphs (visualisations)' },
 		{ l: 'comments',		t: 'Comments' },
 	];
 	
@@ -84,4 +84,104 @@ xpapp.controller('FeedbackController', ['$controller', '$scope', '$rootScope', '
 	// Taxonomy UI
 	$scope.taxonomy = [];
 	$scope.taxonomy.push.apply($scope.taxonomy, taxonomy);
+	
+	// Graphs UI
+	$scope.graphs =  [
+		{ // 00
+			l: 'Breakdown of cost by cause of locality misses',
+			desc: '',
+			c: [taxonomy[4]]
+		}, { // 01
+			l: 'Proportion of cache line invalidations',
+			desc: 'A cache line invalidation can occur when multiple cores modify a shared memory location',
+			c: [taxonomy[2], taxonomy[5]]
+		}, { // 02
+			l: 'Proportion of locality misses',
+			desc: 'Data cache misses as a proportion of instructions executed',
+			c: [taxonomy[2], taxonomy[4], taxonomy[5]]
+		}, { // 03
+			l: 'Remote memory access by core',
+			desc: 'Memory bandwidth',
+			c: [taxonomy[2], taxonomy[5]]
+		}, { // 04
+			l: 'Idle cores',
+			desc: 'Times that cores are idle',
+			c: [taxonomy[3]]
+		}, { // 05
+			l: 'Breakdown of cache line invalidations by core',
+			desc: '',
+			c: [taxonomy[2], taxonomy[5]]
+		}, { // 06
+			l: 'Execution phases',
+			desc: 'phase of execution (sequential or parallel)',
+			c: [taxonomy[3]]
+		}, { // 07
+			l: 'Lock contentions',
+			desc: 'Locking with and without contention',
+			c: [taxonomy[1], taxonomy[5]]
+		}, { // 08
+			l: 'Time waiting for a lock',
+			desc: '',
+			c: [taxonomy[1], taxonomy[2]]
+		}, { // 09
+			l: 'Remote memory access',
+			desc: 'Memory bandwidth',
+			c: [taxonomy[2], taxonomy[5]]
+		}, { // 10
+			l: 'Chains of dependencies on locks',
+			desc: 'synchronisations and waiting between threads',
+			c: [taxonomy[2], taxonomy[3]]
+		}, { // 11a
+			l: 'Migrations by thread (rate)',
+			desc: 'creation, running, moving between cores, termination',
+			c: [taxonomy[0]]
+		}, { // 11b
+			l: 'Migrations by thread (events)',
+			desc: 'creation, running, moving between cores, termination',
+			c: [taxonomy[0]]
+		}, { // 11c
+			l: 'Migrations by thread (core affinity)',
+			desc: 'creation, running, moving between cores, termination',
+			c: [taxonomy[0]]
+		}, { // 12
+			l: 'Time each thread spends waiting for locks',
+			desc: '',
+			c: [taxonomy[1]]
+		}, { // 13
+			l: 'Rate of thread migrations',
+			desc: 'thread switching the core on which it is executing',
+			c: [taxonomy[0], taxonomy[3]]
+		}, { // 14
+			l: 'Breakdown of thread states compared to number of cores',
+			desc: 'number of threads compared to number of cores',
+			c: [taxonomy[0]]
+		}, { // 15
+			l: 'Breakdown of thread states compared to number of cores',
+			desc: 'number of threads compared to number of cores',
+			c: [taxonomy[3]]
+		}, { // 16
+			l: 'Core switching the thread it is executing',
+			desc: 'thread switches',
+			c: [taxonomy[0]]
+		},
+	];
+	$scope.graphs.forEach(function(element) {
+		element.p = [];
+		element.c.forEach(function(tax, index_tax) {
+			tax.s.forEach(function(issue, index_issue) {
+				element.p.push({
+					id: (index_tax + 1) + '_' + (index_issue + 1),
+					l: issue + ' (' + tax.l + ')' });
+			}, this);
+		}, this);
+	}, this);
+	
+	// Comments UI
+	$scope.comments = [
+		{ l: 'Overall appreciation' },
+		{ l: 'Unexpected or surprising elements' },
+		{ l: 'Expected (predictable) things' },
+		{ l: 'Missing features' },
+		{ l: 'Free comments' },
+	];
 }]);
